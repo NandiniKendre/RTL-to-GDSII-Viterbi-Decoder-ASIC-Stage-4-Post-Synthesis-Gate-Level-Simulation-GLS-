cab90e9b04ecdfa9f8713cbbdb39aae13131722a
Nandini Kendre <nandinikendre08@gmail.com>
2026-06-17 10:24:00 +0530

    Add files via upload

---
+++ Unit Delay GLS/unit_delay_simulation.f
@@ -0,0 +1,23 @@
+-gui
+
+/run/media/ccoew/_DATA/cadence/Viterbi_Decoder/synthesis/output_files/viterbi_k7_incremental.v 
+
+/run/media/ccoew/_DATA/cadence/Viterbi_Decoder/Unit_Delay_Simulation/Tb_viterbi_withpad.v
+
+-v /run/media/ccoew/_DATA/cadence/SCLPDK_V3.0_KIT/scl180/stdcell/fs120/6M1L/verilog/vcs_sim_model/tsl18fs120_scl.v
+
+-v /run/media/ccoew/_DATA/cadence/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3c01.v
+ 
+-v /run/media/ccoew/_DATA/cadence/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3d01.v
+
+-v /run/media/ccoew/_DATA/cadence/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3o05.v
+
+-relax
+
+-timescale 1ns/10ps
+
+-mess
+
++delay_mode_unit
+
+-access +rwc
