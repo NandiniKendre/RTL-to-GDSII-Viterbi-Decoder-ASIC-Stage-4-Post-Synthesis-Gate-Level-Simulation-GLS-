862b34f065ae30ced86cbad3069058864a41d53a
Nandini Kendre <nandinikendre08@gmail.com>
2026-06-17 10:27:05 +0530

    Add files via upload

---
+++ SDF Annotated GLS/sdf_simulation.f
@@ -0,0 +1,25 @@
+-gui
+
+/run/media/ccoew/_DATA/cadence/Viterbi_Decoder/synthesis/output_files/viterbi_k7_incremental.v 
+
+/run/media/ccoew/_DATA/cadence/Viterbi_Decoder/03_sdf_simulation/viterbi_tb_with_pad.v
+
+-v /run/media/ccoew/_DATA/cadence/SCLPDK_V3.0_KIT/scl180/stdcell/fs120/6M1L/verilog/vcs_sim_model/tsl18fs120_scl.v
+
+-v /run/media/ccoew/_DATA/cadence/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3c01.v
+ 
+-v /run/media/ccoew/_DATA/cadence/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3d01.v
+
+-v /run/media/ccoew/_DATA/cadence/SCLPDK_V3.0_KIT/scl180/iopad/cio150/6M1L/verilog/tsl18cio150/zero/pc3o05.v
+
+-relax
+
+-sdf_verbose
+
+-timescale 1ns/10ps
+
+-sdfstats stats.txt
+
+-mess
+
+-access +rwc
